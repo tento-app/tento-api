91d50f922ba042788db1a2086606549b712aca9d
ergofriend <altsir.cgc.yhyavr@gmail.com>
2019-02-18 02:18:55 +0900

    UpdateProject mutation

--- gql/schema.py
+++ gql/schema.py
@@ -3,6 +3,7 @@ import graphene
 from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
+from graphql_relay.node.node import from_global_id
 
 from graphql_jwt.decorators import login_required
 
@@ -93,9 +94,34 @@ class CreateProject(graphene.Mutation):
             content=project_data.content,
             user=info.context.user
         )
-        # for tag in project_data.tags:
-        #     project.tags.add(Tag.objects.get(name=tag))
+        if project_data.tags:
+            for tag in project_data.tags:
+                project.tags.add(Tag.objects.get(name=tag))
         return CreateProject(project=project)
+
+class UpdateProject(graphene.Mutation):
+    class Arguments:
+        project_id  = graphene.String(required=True)
+        project_data = ProjectInput()
+        token  = graphene.String(required=True)
+
+    project = graphene.Field(ProjectNode)
+
+    @staticmethod
+    @login_required
+    def mutate(root, info, token=None, project_data=None, project_id=None):
+        db_id = from_global_id(project_id)
+        print(db_id[1])
+        try:
+            project = Project.objects.get(pk=db_id[1])
+            project.name = project_data.name
+            project.save()
+            if project_data.tags:
+                for tag in project_data.tags:
+                    project.tags.add(Tag.objects.get(name=tag))
+        except Project.model.DoesNotExist:
+            return None
+        return UpdateProject(project=project)
 
 class Mutation(graphene.ObjectType):
     create_project = CreateProject.Field()
