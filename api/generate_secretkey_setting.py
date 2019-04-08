0b9848f16f75bbe939e3d3b37625ba27630ba3de
ergofriend <altsir.cgc.yhyavr@gmail.com>
2019-04-09 00:56:38 +0900

    change SCRET_KEY

---
+++ api/generate_secretkey_setting.py
@@ -0,0 +1,7 @@
+from django.core.management.utils import get_random_secret_key
+
+secret_key = get_random_secret_key()
+text = 'SECRET_KEY = \'{0}\''.format(secret_key)
+print(text)
+
+# python generate_secretkey_setting.py > local_settings.py
